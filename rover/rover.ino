7bd68f5c09bcc686b25f9f8eca96c2479cf7fc02
Ben Ferguson <bfergusonx64@outlook.com>
2020-01-15 01:12:53 -0500

    Added support for getting lidar data
            -Added configuration for VL53L0X and method for returning the
    	current distance reading in millimeters

--- rover/rover.ino
+++ rover/rover.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+/*---------------------------------------------------------------
+*	rover.ino
+*	Author: Ben Ferguson
+*	Date created: 1-14-20
+*	Date modified: 1-14-20
+*	Description: main logic for the rover micro
+---------------------------------------------------------------*/
 
+#include <Adafruit_VL53L0X.h>
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+long lidarGetRangemm()
+{
+	VL53L0X_RangingMeasurementData_t measure;
+
+	lox.rangingTest(&measure, false);
+
+	if (measure.RangeStatus != 4) {
+		return measure.RangeMilliMeter;
+	} 
+
+	else {
+		return -1;
+	}
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial){
+  	delay(1);
+  }
+
+  Serial.println(F("Initializing..."));
+
+  Serial.print(F("Initialzing VL53L0X..."));
+
+  if (!lox.begin()) {
+    Serial.println(F("FAIL"));
+    while(1);
+  }
+
+  Serial.println(F("DONE"));
+
+  Serial.println(F("Initialization complete!"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
