93c34e77f48de778fe916e30f08e1cda8ff47db7
Ben Ferguson <bfergusonx64@outlook.com>
2020-02-19 00:56:28 -0500

    libradio library working
    -Library is now portable and supports easy plug-and-play usage of radio
    module
    -Simply include the library, run the init method, then send and receive
    data using the "send" and "receive" methods

--- src/libradio/libradio.cpp
+++ src/libradio/libradio.cpp
@@ -4,15 +4,19 @@ namespace radio
 {
 	RH_RF95 *rf95;
 
+	uint8_t recvBuffer[RH_RF95_MAX_MESSAGE_LEN];
+
+	uint8_t r_len;
+
 	uint8_t resetPin;
 
-	void init(uint8_t rst, uint8_t cst, uint8_t intr, unsigned double freq)
+	void init(uint8_t rst, uint8_t cst, uint8_t intr, double freq)
 	{
 		resetPin = rst;
 
 		rf95 = new RH_RF95(cst, intr);
 
-		pinMode(rst, output);
+		pinMode(rst, OUTPUT);
 		digitalWrite(rst, HIGH);
 
 		digitalWrite(rst, LOW);
@@ -20,24 +24,43 @@ namespace radio
 
 		Serial.print("Initializing radio... ");
 
-	    if (!rf95.init()) 
+	    if (!rf95->init()) 
 	    {
 	    	Serial.println("FAIL: Unable to init radio module");
 	        while(1);
 	    }
 
-	    if (!rf95.setFrequency(RF95_FREQ)) 
+	    if (!rf95->setFrequency(freq)) 
 	    {
-	    	Serial.println("FAIL: Unable to set frequency")
+	    	Serial.println("FAIL: Unable to set frequency");
 	        while(1);
 	    }
 	}
 
 	void reset(unsigned int duration)
 	{
-	    digitalWrite(RFM95_RST, LOW);
+	    digitalWrite(resetPin, LOW);
 	    delay(duration);
-	    digitalWrite(RFM95_RST, HIGH); 
+	    digitalWrite(resetPin, HIGH); 
 	   	delay(duration);
+	}
+
+	uint8_t receive()
+	{
+		if (rf95->waitAvailableTimeout(1000)) 
+		{
+	        int packId;
+
+	        rf95->recv(recvBuffer, &r_len);
+
+	        return 1;
+   		}
+
+   		return 0;
+	}
+
+	uint8_t send(void *buffer, uint8_t size)
+	{
+		return rf95->send((uint8_t *)buffer, size) && rf95->waitPacketSent();
 	}
 }
--- src/libradio/libradio.h
+++ src/libradio/libradio.h
@@ -3,15 +3,25 @@
 
 #include <RH_RF95.h>
 
+
+
 namespace radio
 {
-	RH_RF95 *rf95;
+	extern RH_RF95 *rf95;
 
-	uint8_t resetPin;
+	extern uint8_t recvBuffer[RH_RF95_MAX_MESSAGE_LEN];
+
+	extern uint8_t r_len;
+
+	extern uint8_t resetPin;
 	
-	void init(uint8_t rst, uint8_t cst, uint8_t intr, unsigned double freq);
+	void init(uint8_t rst, uint8_t cst, uint8_t intr, double freq);
 
 	void reset(unsigned int duration);
+
+	uint8_t receive();
+
+	uint8_t send(void *buffer, uint8_t size);
 }
 
 #endif
