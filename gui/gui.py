27ce17bf2e60869e608581d12dcc111d9e4aaaa8
Robert Max Williams <robertmaxwilliams@gmail.com>
2020-03-03 19:32:51 -0500

    copied in gui stuff from gui branch

--- gui/gui.py
+++ gui/gui.py
@@ -1,15 +1,42 @@
 import tkinter as tk
 import serial
+import struct
 
-base_station = serial.Serial("/dev/cu.usbmodem14231")
+base_station = serial.Serial("/dev/cu.usbmodem1421", timeout=0.1)
 
+
+def send_joystick(x1, y1, x2, y2):
+    packet = struct.pack('iffff', 1, x1, y1, x2, y2)
+    base_station.write(b'%c' % len(packet))
+    base_station.write(packet)
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+
+
+        self.text_box = tk.Text(master=master)
+        self.text_box.pack(side=tk.RIGHT)
+        self.text_box.config(state="disabled")
+        
+
+        master.bind("<KeyPress>", lambda e: self.keydown(e))
         self.pack()
         self.create_widgets()
+    def keydown(self, e):
+        print('down', e.char)
+        c = e.char
+        if c == 'w':
+            send_joystick(0, 1, 0, 0);
+        elif c == 'a':
+            send_joystick(1, 0, 0, 0);
+        elif c == 's':
+            send_joystick(0, -1, 0, 0);
+        elif c == 'd':
+            send_joystick(-1, 0, 0, 0);
+        else:
+            print('not revognised by bui')
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
@@ -22,7 +49,12 @@ class Application(tk.Frame):
         self.quit.pack(side="bottom")
 
     def say_hi(self):
+        foo = base_station.readline().decode('utf-8') 
+        self.text_box.config(state="normal")
+        self.text_box.insert(tk.END, foo)
+        self.text_box.config(state="disabled")
         print("hi there, everyone!")
+        #self.text_box.insert(tk.END, "Hello!\n")
         base_station.write(b"w")
 
 root = tk.Tk()
